733888f5e26720831687e0555b5a0663b28efe0a
playbar <hgl868@126.com>
2018-07-30 09:56:48 +0800

    添加cmake 文件，目前编译错误

--- include/internal/cryptlib.h
+++ include/internal/cryptlib.h
@@ -58,11 +58,11 @@ DEFINE_LHASH_OF(MEM);
 # define OPENSSL_CONF             "openssl.cnf"
 
 # ifndef OPENSSL_SYS_VMS
-#  define X509_CERT_AREA          OPENSSLDIR
-#  define X509_CERT_DIR           OPENSSLDIR "/certs"
-#  define X509_CERT_FILE          OPENSSLDIR "/cert.pem"
-#  define X509_PRIVATE_DIR        OPENSSLDIR "/private"
-#  define CTLOG_FILE              OPENSSLDIR "/ct_log_list.cnf"
+#  define X509_CERT_AREA           "./"
+#  define X509_CERT_DIR            "./certs"
+#  define X509_CERT_FILE           "./cert.pem"
+#  define X509_PRIVATE_DIR         "./private"
+#  define CTLOG_FILE               "./ct_log_list.cnf"
 # else
 #  define X509_CERT_AREA          "OSSL$DATAROOT:[000000]"
 #  define X509_CERT_DIR           "OSSL$DATAROOT:[CERTS]"
